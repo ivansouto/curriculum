b2e81b281f1d5c81b0638a9b2193693fc564874d
ivansouto <45987972+ivansouto@users.noreply.github.com>
2021-08-24 22:47:26 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,33 @@
 
-$.ajax({
-  url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSmuHLlBk5Ts6VPcVSOT3TKda4pyfbhpeZGumrttC3cscJ1OhwHlRhOZvdQeKw6Mq-vH1ezwf-uPub7/pub?output=csv',
-  dataType: 'text',
-}).done(successFunction);
+//$.ajax({
+//  url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSmuHLlBk5Ts6VPcVSOT3TKda4pyfbhpeZGumrttC3cscJ1OhwHlRhOZvdQeKw6Mq-vH1ezwf-uPub7/pub?output=csv',
+//  dataType: 'text',
+//}).done(successFunction);
 
-function successFunction(data) {
- console.log(data);
+var files = ["'https://docs.google.com/spreadsheets/d/e/2PACX-1vSmuHLlBk5Ts6VPcVSOT3TKda4pyfbhpeZGumrttC3cscJ1OhwHlRhOZvdQeKw6Mq-vH1ezwf-uPub7/pub?output=csv'"];
+var allResults = [];
+
+for (var i = 0; i < files.length; i++)
+{
+    Papa.parse(files[i], {
+        download: true,
+        header: true,
+        skipEmptyLines: true,
+        error: function(err, file, inputElem, reason) { /* handle*/ },
+        complete: function(results) {
+            allResults.push(results);
+            if (allResults.length == files.length)
+            {
+              console.log(allResults);
+                // Do whatever you need to do
+            }
+        }
+    });
 }
+
+//function successFunction(data) {
+// console.log(data);
+//}
 
 function setup() {
   createCanvas(1000, 400);
